64dfb6a2563d905603390b19f10bd91b2d761bfd
Muhammad <muhammadhullah23@gmail.com>
2021-10-03 19:47:01 -0400

    fetch request in RemedyApi get all the data. Call it in index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const port = `http://localhost:3000` ;
 const remedyApiCall = new RemedyApi(port);
 // to call different fetch requests. remedyApiCall is an instance to call an instance method
+
+remedyApiCall.getRemedies()
--- src/remedy_api.js
+++ src/remedy_api.js
@@ -2,4 +2,11 @@ class RemedyApi {
     constructor(port){
         this.port = port
     }
+
+    getRemedies(){
+        fetch(this.port + `/remedies`) 
+        .then(r => r.json())
+        .then(remedies => console.log(remedies))
+    }
 }
+ 
