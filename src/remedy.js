d2e53ba7f24f68c5a9cddb5c3cbd20187d12c17b
Muhammad <muhammadhullah23@gmail.com>
2021-10-07 18:28:09 -0400

    render/disply info structure of remedy in the DOM

--- src/remedy.js
+++ src/remedy.js
@@ -8,7 +8,18 @@ class Remedy {
         this.illness = illness
         this.illness_id = illness_id
         this.element = document.createElement('li');
-        this.element.id = `remedy-${id}`
-        debugger
+        this.element.id = `remedy-${id}` 
+        // debugger
+    }
+
+    displayInfo(){
+        return this.element.innerHTML = `
+        <div id="remedy-${this.id}">
+        <h3 class="name">${this.name}</h3>
+        <p class="benfits">${this.benefits}</p>
+        <p class="ingredients">${this.ingredients}</p>
+        <p class="steps">${this.steps}</p>
+        </div>
+        `
     }
 }
