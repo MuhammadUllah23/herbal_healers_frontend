603c719eb62a657ff3e2247992c919dea92fd7b8
Muhammad <muhammadhullah23@gmail.com>
2021-10-09 18:45:42 -0400

    declare 'form' and 'dropDown' variable that grabs the form via id and drop down in the form via id

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const port = `http://localhost:3000` ;
 const remedyApiCall = new RemedyApi(port);
+const form = document.getElementById("remedy-form")
+const dropDown = document.getElementById("illness-dropdown")
 // to call different fetch requests. remedyApiCall is an instance to call an instance method
 
 remedyApiCall.getRemedies()
