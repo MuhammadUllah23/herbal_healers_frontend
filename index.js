0f78dae20271fa9a5d8358ddc75a1c1fe6f32ae1
Muhammad <muhammadhullah23@gmail.com>
2021-10-09 18:51:37 -0400

    submit event listener for form. handle submit function to end to database

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const port = `http://localhost:3000` ;
 const remedyApiCall = new RemedyApi(port);
-const form = document.getElementById("remedy-form")
-const dropDown = document.getElementById("illness-dropdown")
 // to call different fetch requests. remedyApiCall is an instance to call an instance method
+const form = document.getElementById("remedy-form");
+const dropDown = document.getElementById("illness-dropdown");
+
 
 remedyApiCall.getRemedies()
+
+form.addEventListener('submit' handleSubmit)
+
+function handleSubmit(e){
+    e.preventDefault()
+    
+}
