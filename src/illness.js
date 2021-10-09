c44d2c9dcf68a1c6235d36a3a6a7ab33d46674f5
Muhammad <muhammadhullah23@gmail.com>
2021-10-09 19:45:31 -0400

    Illness constructor in illness.js

--- src/illness.js
+++ src/illness.js
@@ -1,3 +1,8 @@
 class Illness {
-    
+    constructor({name, symptoms, id, remedies}) {
+        this.name = name
+        this.symptoms = symptoms
+        this.id = id
+        this.remedies = remedies
+    }
 }
